1595f3c9767c8ccb36ba5f565228ee71549343cc
Parth Jindal <pr902410@dal.ca>
2024-11-21 16:26:08 -0400

    dded changes

--- src/test/java/com/example/hello/HelloControllerTests.java
+++ src/test/java/com/example/hello/HelloControllerTests.java
@@ -23,7 +23,7 @@ public class HelloControllerTests {
     public void welcome_ok() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Hello World!")));
+                .andExpect(content().string(equalTo("Hello World! This is Springboot.")));
     }
 
 }
